12b39af87c02f8107c34a0a9aaa52db11e2f3565
itsshashanksp <9945shashank@gmail.com>
2024-06-01 15:42:38 +0530

    Merge branch 'master' of https://github.com/itsshashanksp/KernelSU into sleepy
    Signed-off-by: Shashank Patil <9945shashank@gmail.com>

--- drivers/kernelsu/ksu.h
+++ drivers/kernelsu/ksu.h
@@ -4,7 +4,7 @@
 #include <linux/types.h>
 #include <linux/workqueue.h>
 
-#define KERNEL_SU_VERSION 11838
+#define KERNEL_SU_VERSION 11872
 #define KERNEL_SU_OPTION 0xDEADBEEF
 
 #define CMD_GRANT_ROOT 0
